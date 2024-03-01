7b105940dfa9f90011e0b5e55f8df9b9a7483815
PraiseImmanuel <praiseimmanuel601@gmail.com>
2024-03-01 02:47:46 +0100

    Add cors

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.post("/api/email/send-email", async (req, res) => {
   try {
